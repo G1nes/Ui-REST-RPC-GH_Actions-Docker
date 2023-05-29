ff08fa3491d06241764ddf31416107c49810dc29
timur.salakhov <extern.salakhov_timur@allianz.com>
2023-05-30 03:32:00 +0400

    trying to authorize via notification window

--- src/main/java/com/example/ui/metamask/MetamaskFlow.java
+++ src/main/java/com/example/ui/metamask/MetamaskFlow.java
@@ -88,10 +88,10 @@ public class MetamaskFlow {
 
     public void authorize() {
         //Selenide.webdriver().object().getWindowHandles().forEach(log::info);
-        //Selenide.switchTo()
-        //        .window(appConfiguration.extensionNotification(),
-        //                Duration.ofMillis(Configuration.pageLoadTimeout));
-        Selenide.open("chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/notification.html");
+        Selenide.switchTo()
+                .window(appConfiguration.extensionNotification(),
+                        Duration.ofMillis(Configuration.pageLoadTimeout));
+        //Selenide.open("chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/notification.html");
         $x("//input[@data-testid='unlock-password']").sendKeys(PASSWORD);
         $x("//button[@data-testid='unlock-submit']").click();
         Selenide.switchTo().window(appConfiguration.appName());
