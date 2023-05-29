dd7fed9d080e0eda4c6b62da931fbca9b8b96c03
timur.salakhov <extern.salakhov_timur@allianz.com>
2023-05-29 08:31:49 +0400

    add logging

--- src/main/java/com/example/ui/configuration/SelenideConfiguration.java
+++ src/main/java/com/example/ui/configuration/SelenideConfiguration.java
@@ -2,8 +2,13 @@ package com.example.ui.configuration;
 
 import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
+import java.util.logging.Level;
 
 public class SelenideConfiguration {
 
@@ -14,15 +19,22 @@ public class SelenideConfiguration {
     }
 
     public void configure() {
-        Configuration.baseUrl = configuration.browserUrl();
-        Configuration.browserSize = configuration.browserResolution();
-        Configuration.timeout = Long.parseLong(configuration.browserElementTimeoutMillis());
-        Configuration.pageLoadTimeout = Long.parseLong(configuration.browserPageTimeoutMillis());
+        LoggingPreferences loggingPreferences = new LoggingPreferences();
+        loggingPreferences.enable(LogType.BROWSER, Level.INFO);
+        loggingPreferences.enable(LogType.DRIVER, Level.INFO);
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("loggingPrefs", loggingPreferences);
         ChromeOptions chromeOptions = new ChromeOptions()
                 .addExtensions(new File("src/main/resources/10.30.4_0.crx"))
                 .addArguments("--no-sandbox")
                 .addArguments("--disable-dev-shm-usage")
                 .addArguments("--headless");
+        chromeOptions.setCapability(ChromeOptions.CAPABILITY, desiredCapabilities);
+
         Configuration.browserCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+        Configuration.baseUrl = configuration.browserUrl();
+        Configuration.browserSize = configuration.browserResolution();
+        Configuration.timeout = Long.parseLong(configuration.browserElementTimeoutMillis());
+        Configuration.pageLoadTimeout = Long.parseLong(configuration.browserPageTimeoutMillis());
     }
 }
