ad123dc9e4fcc13bcd30ce3361f900831df896e3
timur.salakhov <extern.salakhov_timur@allianz.com>
2023-05-29 12:17:12 +0400

    fix test

--- src/main/java/com/example/ui/elements/CreateCollectionForm.java
+++ src/main/java/com/example/ui/elements/CreateCollectionForm.java
@@ -14,8 +14,7 @@ public class CreateCollectionForm {
     private final SelenideElement createButton;
 
     public CreateCollectionForm() {
-        SelenideElement container = $x("//b[contains(text(), 'Deploy Collection')]/following-sibling::form")
-                .shouldBe(Condition.visible);
+        SelenideElement container = $x("//b[contains(text(), 'Deploy Collection')]/following-sibling::form");
         collectionNameInput = container.$x(".//input[@placeholder = 'Enter collection name']");
         collectionSymbolInput = container.$x(".//input[@placeholder = 'Enter collection symbol']");
         collectionTokenUriInput = container.$x(".//input[@placeholder = 'Enter collection token URI']");
