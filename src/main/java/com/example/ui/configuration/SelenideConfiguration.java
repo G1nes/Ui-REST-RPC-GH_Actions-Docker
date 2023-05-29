df902ed67896bfd013f31f300ff92a1506ddd700
G1nes <45970920+G1nes@users.noreply.github.com>
2023-05-29 09:35:33 +0400

    Screenshots and ci (#5)
    * add chrome options
    
    * add logging
    
    * fix headless mode
    
    * fix headless mode
    
    * fix headless mode
    
    * fix headless mode
    
    ---------

--- src/main/java/com/example/ui/configuration/SelenideConfiguration.java
+++ src/main/java/com/example/ui/configuration/SelenideConfiguration.java
@@ -2,12 +2,8 @@ package com.example.ui.configuration;
 
 import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.logging.LogType;
-import org.openqa.selenium.logging.LoggingPreferences;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
-import java.util.logging.Level;
 
 public class SelenideConfiguration {
 
@@ -22,6 +18,7 @@ public class SelenideConfiguration {
                 .addExtensions(new File("src/main/resources/10.30.4_0.crx"))
                 .addArguments("--no-sandbox")
                 .addArguments("--headless=new")
+                .addArguments("--disable-gpu")
                 .addArguments("--disable-dev-shm-usage");
 
         Configuration.browserCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
@@ -30,5 +27,6 @@ public class SelenideConfiguration {
         Configuration.timeout = Long.parseLong(configuration.browserElementTimeoutMillis());
         Configuration.pageLoadTimeout = Long.parseLong(configuration.browserPageTimeoutMillis());
         Configuration.webdriverLogsEnabled = true;
+        Configuration.headless = true;
     }
 }
