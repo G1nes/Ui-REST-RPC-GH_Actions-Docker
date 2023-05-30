c7bcd878fdb432213c849b6eb3bb2db656d3ca06
timur.salakhov <extern.salakhov_timur@allianz.com>
2023-05-30 05:30:08 +0400

    added Rpc client

--- src/main/java/com/example/api/clients/rpc/JsonRpsResponseDto.java
+++ src/main/java/com/example/api/clients/rpc/JsonRpsResponseDto.java
@@ -1,8 +1,10 @@
 package com.example.api.clients.rpc;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class JsonRpsResponseDto {
     private String jsonrpc;
     private Long id;
